37515533f72a67455a487d1a674afe30193a5024
TITAN\israel <blas@irakasle.eus>
2025-09-26 19:20:49 +0200

    DOC mejora la explicación de esta clase

--- src/main/java/es/israeldelamo/demomariadb/Lanzador.java
+++ src/main/java/es/israeldelamo/demomariadb/Lanzador.java
@@ -2,9 +2,14 @@ package es.israeldelamo.demomariadb;
 
 /**
  * Punto de entrada en la aplicación
+ * @author Israel
+ * @version 1.0
  */
 public class Lanzador {
     public static void main(String[] args) {
+        // una medida de seguridad es usar esta clase lanzadora
+        // como no extiende a application es menos probable que falle
+        // solo sirve para lanzar la otra clase
         AdministradorPersonas.launch(AdministradorPersonas.class);
     }
 }
