4f85bba7ee8d2dd7674e58a80118fa43bbf89b22
TITAN\israel <blas@irakasle.eus>
2025-09-30 19:43:51 +0200

    FIX faltaba traduccioón en las alertas del controlador

--- src/main/java/es/israeldelamo/demomariadb/controladores/ControladorMuestraPersonas.java
+++ src/main/java/es/israeldelamo/demomariadb/controladores/ControladorMuestraPersonas.java
@@ -1,5 +1,6 @@
 package es.israeldelamo.demomariadb.controladores;
 
+import es.israeldelamo.demomariadb.AdministradorPersonas;
 import es.israeldelamo.demomariadb.dao.DaoDni;
 import es.israeldelamo.demomariadb.modelos.ModeloPersona;
 import es.israeldelamo.demomariadb.util.Alertas;
@@ -7,16 +8,29 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.Locale;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
 
 /**
  * Una clase controladora de ejemplo para muestraPersonas.fxml
  */
 
 public class ControladorMuestraPersonas {
-       /**
+
+    /**
+     * El bundle de idiomas para este controlador
+     */
+    private static ResourceBundle bundle;
+
+    /**
      * TAbla de personas
      */
     @FXML
@@ -44,7 +58,8 @@ public class ControladorMuestraPersonas {
      * Procedimiento para rellenar la tabla
      */
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onActualizarButton() {
+
         rellenarTabla();
     }
 
@@ -52,10 +67,24 @@ public class ControladorMuestraPersonas {
      * Se dispara cada vez que se carga la ventana
      */
     public void initialize() {
+        cargarBundleIdiomas();
         //cuando la queremos async
         rellenarTablaAsync();
         // cuando la queremos sync
         //  rellenarTabla();
+    }
+
+
+    /**
+     * Carga el bundle de idiomas para que aparezcan las traducciones
+     * podriamos haberlo traido de la clase anterior?
+     */
+    private void cargarBundleIdiomas(){
+        // Cargar el idioma seleccionado
+        Preferences prefs = Preferences.userNodeForPackage(AdministradorPersonas.class);
+        String idioma = prefs.get("textos", "es"); // Valor por defecto "es"
+        Locale locale = new Locale(idioma);
+        bundle = ResourceBundle.getBundle("textos", locale);
     }
 
     /**
@@ -111,4 +140,68 @@ public class ControladorMuestraPersonas {
         Alertas alertaAcercaDe = new Alertas();
         alertaAcercaDe.mostrarInformacion("Version 1.0 de la demo");
     }
+
+
+    /**
+     * Muestra una alerta de confirmación antes de salir del programa
+     */
+    @FXML
+    private void onArchivoCerrar(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Confirmación");
+        alert.setContentText(bundle.getString("AlertaSalir"));
+        Optional<ButtonType> action = alert.showAndWait();
+        if (action.isPresent() && action.get() == ButtonType.OK) {
+            Platform.exit();
+        }
+    }
+
+
+    /**
+     * Muestra una alerta de confirmación antes de eliminar una linea seleccionada del tableview
+     */
+    @FXML
+    private void onEdicionEliminar(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Confirmación");
+        alert.setContentText(bundle.getString("AlertaEliminar"));
+        Optional<ButtonType> action = alert.showAndWait();
+        if (action.isPresent() && action.get() == ButtonType.OK) {
+
+            eliminarFIlasSeleccionadasDeBBDD();
+            eliminarFilasSeleccionadasDeTV();
+
+        }
+
+
+    }
+
+    /**
+     * Elimina las filas seleccionadas del tableview
+     */
+    private void eliminarFilasSeleccionadasDeTV() {
+        tvDni.getSelectionModel().getSelectedItems().forEach(tvDni.getItems()::remove);
+    }
+
+    /**
+     * Elimina las filas seleccionadas del tableview
+     */
+
+    private void eliminarFIlasSeleccionadasDeBBDD() {
+        tvDni.getSelectionModel().getSelectedItems().forEach(persona -> {
+            DaoDni.eliminarPersonaAsync(persona).thenAccept(result -> {
+                if (result) {
+                    Alertas alerta = new Alertas();
+                    alerta.mostrarInformacion(bundle.getString("AlertaEliminacionExitosa"));
+                } else {
+                    Alertas alerta = new Alertas();
+                    alerta.mostrarInformacion(bundle.getString("AlertaEliminacionFallida"));
+                }
+            });
+        });
+    }
+
+
 }
